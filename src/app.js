8f223102b4b4832a957688f71a3231e444502990
Vinayak Suthar <sutharvinayak28@gmail.com>
2025-10-07 11:03:19 +0530

    feat: refactor routes with file based routing

--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
 const express = require('express');
-const healthRouter = require('./routes/health');
 const { zodErrorMiddleware } = require('./middlewares/zodError');
 const { authStub } = require('./middlewares/authStub');
+const registerRoutes = require('./routes');
 
 const app = express();
 app.use(express.json({ limit: '1mb' }));
 app.use(authStub); // replace with real auth later
 
-app.use('/health', healthRouter);
-require('./routes/user-routes')(app);
-require('./routes/code-routes')(app);
+registerRoutes(app);
 
 app.use(zodErrorMiddleware);
 const errorHandler = require('./middlewares/error-handler');
